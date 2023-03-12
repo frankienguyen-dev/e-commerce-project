5e41fc91839ba275c034fc441b8e921695c0739e
frankienguyen-dev <binguyen221198@gmail.com>
2023-03-12 07:30:15 +0700

    Handle error code 422 from server

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -5,6 +5,13 @@ import Input from 'src/components/Input';
 import { schema, Schema } from 'src/utils/rules';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from '@tanstack/react-query';
+import { registerAccount } from 'src/apis/auth.api';
+import { omit } from 'lodash';
+import { isAxiosError } from 'axios';
+import { axiosUnprocessableEntityError } from 'src/utils/utils';
+import { ServerResponse } from 'http';
+import { ResponseApi } from 'src/types/utils.type';
 
 type formData = Schema;
 
@@ -12,21 +19,52 @@ export default function Register() {
   const {
     handleSubmit,
     register,
+    setError,
     getValues,
     formState: { errors }
   } = useForm<formData>({
     resolver: yupResolver(schema)
   });
 
-  const onSubmit = handleSubmit(
-    (data) => {
-      // console.log('check data: ', data);
-    },
-    (data) => {
-      const password = getValues('password');
-      console.log(password);
-    }
-  );
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<formData, 'password_confirm'>) => registerAccount(body)
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    const body = omit(data, ['password_confirm']);
+    registerAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        console.log('check data when success: ', data);
+      },
+
+      onError: (error) => {
+        if (axiosUnprocessableEntityError<ResponseApi<Omit<formData, 'password_confirm'>>>(error)) {
+          const formError = error.response?.data.data;
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof Omit<formData, 'password_confirm'>, {
+                message: formError[key as keyof Omit<formData, 'password_confirm'>],
+                type: 'Server'
+              });
+            });
+          }
+          // if (formError?.email) {
+          //   setError('email', {
+          //     message: formError.email,
+          //     type: 'Server'
+          //   });
+          // }
+
+          // if (formError?.password) {
+          //   setError('password', {
+          //     message: formError.password,
+          //     type: 'Server'
+          //   });
+          // }
+        }
+      }
+    });
+  });
 
   return (
     <div className='bg-orange'>
